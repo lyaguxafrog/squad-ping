8887054b95dc27bcb870e71885526e51d711c471
pr1nce55 <maccree.a@yandex.ru>
2021-11-09 18:49:08 +0300

    add openfile deffunc

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import telebot
 
-bot = telebot.TeleBot()
+import botconfig
+
+bot = telebot.TeleBot(botconfig.TOKEN)
+
+def open_file(path):                  # open file structure for open file like botconfig.py
+    str = ''
+    file = open(path, "r")
+
+    for line in file:
+        str += line
+    
+    return str
+
 
 bot.polling()
 
